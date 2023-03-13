395b99d314c55b9b003a4dda4d52d926e638f5af
Brunswickck <95478953+Brunswickck@users.noreply.github.com>
2023-03-13 16:00:45 +0100

    Revert "added user model, updated form from signup and added validations"

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :description, length: { minimum: 20 }, allow_blank: true
-  validates :phone_number, phone: true
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
