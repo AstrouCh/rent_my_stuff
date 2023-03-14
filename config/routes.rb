36122d2b063ec09c5263568610100e4fccd17811
tom burns <tomburns.gj@gmail.com>
2023-03-14 11:45:56 +0100

    added routes (as a team) lets hope they work

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :items do
+    resources :bookings, only: [:new, :create]
+  end
 
+  resources :bookings, only: [:edit, :update, :destroy, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:edit, :update, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :user, only: [:show]
 end
