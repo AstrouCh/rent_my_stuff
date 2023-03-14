228cde7c558005efe9d02479e47a6f908d46ff01
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-03-14 12:16:25 +0100

    Item controller done

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,51 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: %i[show edit update destroy]
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new, status: :unprocessable_entry
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @item = item.update(params[:item])
+    if @item.update
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entry
+    end
+  end
+
+  def destroy
+    @item.destroy
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :location, :description)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'items/new'
+  get 'items/create'
+  get 'items/edit'
+  get 'items/update'
+  get 'items/destroy'
+  get 'items/show'
+  get 'items/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
