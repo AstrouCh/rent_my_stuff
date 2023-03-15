52482d7f1b6b424b9f550ee7e65bfe80584b33f8
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-03-15 16:34:14 +0100

    geocoding and banner added

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,5 +13,28 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/astrichazaux/clf9pjgvw001j01rx1rpij7f6"
     })
+
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+
+      const customMarker = document.createElement("div")
+      customMarker.innerHTML = marker.marker_html
+
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(this.map)
+    })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 150, maxZoom: 15, duration: 0 })
   }
 }
