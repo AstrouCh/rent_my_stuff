a13afb5a2b40c744a59066e38187241e677a504f
Lloyd Mellor <lloydmellor@gmail.com>
2023-03-15 14:22:43 +0100

    added cloudinary

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :favourites
-
+  has_one_attached :photo
   validates :title, presence: true
   validates :location, presence: true
   validates :description, length: { minimum: 20 }
