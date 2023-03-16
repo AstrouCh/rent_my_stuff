f113b911650dab65bc0dff50b3fa8b3fb9fc38b5
tom burns <tomburns.gj@gmail.com>
2023-03-16 16:48:13 +0000

    updated seeds and user dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :items, dependent: :destroy
   validates :description, length: { minimum: 20 }, allow_blank: true
   validates :phone_number, phone: true
   devise :database_authenticatable, :registerable,
