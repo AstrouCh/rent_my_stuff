34dce113f26bb04b08675434706de0e419c85943
tom burns <tomburns.gj@gmail.com>
2023-03-17 14:40:58 +0000

    some search fixes - STILL broken

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,23 +10,20 @@ class ItemsController < ApplicationController
 
 
   def index
-    if params[:item][:item].present? &&  params[:item][:address].present? && params[:item][:radius].present?
+    @items = Item.all
+    if params.present? && params[:item].present? && params[:item][:item].present? &&  params[:item][:location].present? && params[:item][:radius].present?
       items = Item.search_by_title_and_category_and_description(params[:item][:item])
-      @items = items.near(params[:item][:address], params[:item][:radius])
-    elsif params[:item][:address].present? && params[:item][:radius].present?
-      @items = items.near(params[:item][:address], params[:item][:radius])
-    elsif params[:item][:item].present? &&  params[:item][:address].present?
+      @items = Item.near(params[:item][:location], params[:item][:radius])
+    elsif params.present? && params[:item].present? && params[:item][:location].present? && params[:item][:radius].present?
+      @items = Item.near(params[:item][:location], params[:item][:radius])
+    elsif params.present? && params[:item].present? && params[:item][:item].present? &&  params[:item][:location].present?
       items = Item.search_by_title_and_category_and_description(params[:item][:item])
-      @items = items.near(params[:item][:address], 50)
-    elsif params[:item][:item].present? &&  params[:item][:radius].present?
+      @items = Item.near(params[:item][:location], 50)
+    elsif params.present? && params[:item].present? && params[:item][:item].present? &&  params[:item][:radius].present?
       items = Item.search_by_title_and_category_and_description(params[:item][:item])
-      @items = items.near(current_user.address, params[:item][:radius])
-    elsif params[:item][:item].present?
-      raise
-      items = Item.search_by_title_and_category_and_description(params[:item][:item])
-      @items = items.near(current_user.address, 50)
-    else
-      @items = Item.all
+      @items = Item.near(current_user.address, params[:item][:radius])
+    elsif params.present? && params[:item].present? && params[:item][:item].present?
+      @items = Item.search_by_title_and_category_and_description(params[:item][:item])
     end
     @markers = @items.geocoded.map do |item|
       {
